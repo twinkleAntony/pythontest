a6f9a3ab4657b4f2c0f25f91ec4bf90231a09b8b
twink <twinkleantonysby@gmail.com>
2023-12-19 20:32:20 +0000

    menu driven

---
+++ package.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def install_packages(package_list):
+    """
+    Install a list of packages using apt-get.
+
+    Parameters:
+    - package_list: A list of package names to install.
+
+    Returns:
+    - None
+    """
+    try:
+        # Construct the apt-get install command with the package names
+        install_command = ['sudo', 'apt-get', 'install', '-y'] + package_list
+        subprocess.check_call(install_command)
+        print(f"Successfully installed packages: {', '.join(package_list)}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing packages: {e}")
+
+# List of packages to install
+packages_to_install = [
+    'apt-utils', 'autoconf', 'automake', 'build-essential', 'git',
+    'libcurl4-openssl-dev', 'libgeoip-dev', 'liblmdb-dev', 'libpcre++-dev',
+    'libtool', 'libxml2-dev', 'libyajl-dev', 'pkgconf', 'wget', 'zlib1g-dev'
+]
+
+# Call the function to install the packages
+install_packages(packages_to_install)
