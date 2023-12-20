8825ef48093fa90ebe0d53a20975103ecb56bfc5
twink <twinkleantonysby@gmail.com>
2023-12-20 14:58:06 +0000

    nginx connector

--- Main.py
+++ Main.py
@@ -32,7 +32,7 @@ def compile_modsecurity_nginx_connector():
         subprocess.run(['git', 'clone', '--depth', '1', 'https://github.com/SpiderLabs/ModSecurity-nginx.git'])
 
         # Determine NGINX version
-        nginx_version_process = subprocess.run(['nginx', '-v'], capture_output=True, text=True)
+        nginx_version_process = subprocess.run(['nginx', '-v'])
         nginx_version = nginx_version_process.stdout.split('/')[1].strip()
 
         # Download NGINX source code
@@ -49,9 +49,6 @@ def compile_modsecurity_nginx_connector():
         print("ModSecurity connector for NGINX compiled and installed successfully.")
     except Exception as e:
         print(f"Error during compilation: {e}")
-    finally:
-        # Clean up: Remove the cloned repositories and downloaded files
-        subprocess.run(['rm', '-rf', 'ModSecurity-nginx', f'nginx-{nginx_version}', f'nginx-{nginx_version}.tar.gz'])
 
 
 
