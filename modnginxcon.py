faff8fb160cefe6d3e6bd4626205c7937e0185d2
twinkleAntony <75666358+twinkleAntony@users.noreply.github.com>
2023-12-20 18:04:19 +0000

    Update modnginxcon.py

--- modnginxcon.py
+++ modnginxcon.py
@@ -4,9 +4,10 @@ import shutil
 import urllib.request
 import tarfile
 
-def download_compile_nginx_modsecurity_connector():
-    download_url =  subprocess.run(['sudo', 'git', 'clone', '--depth', '1', 'https://github.com/SpiderLabs/ModSecurity-nginx.git'])
-    download_dir = subprocess.run(['nginx', '-v'])
+def download_compile_nginx_modsecurity_connector(version="v0.1.0"):
+    download_url = f"https://github.com/SpiderLabs/ModSecurity-nginx/archive/{version}.tar.gz"
+    download_dir = f"nginx_modsecurity_connector_{version}"
+
     try:
         # Download and extract
         os.makedirs(download_dir, exist_ok=True)
@@ -23,18 +24,23 @@ def download_compile_nginx_modsecurity_connector():
         module_dir = os.path.join(download_dir, f"ModSecurity-nginx-{version}")
         os.chdir(module_dir)
 
-        subprocess.run(["./configure"])
-        subprocess.run(["make"])
+        # Check if the module directory already exists
+        if os.path.exists("objs/ngx_http_modsecurity_module.so"):
+            print(f"NGINX ModSecurity Connector {version} has already been compiled.")
+        else:
+            subprocess.run(["./configure"])
+            subprocess.run(["make"])
 
-        # Copy the compiled module
-        nginx_modules_dir = "/etc/nginx/modules"
-        shutil.copy("objs/ngx_http_modsecurity_module.so", nginx_modules_dir)
+            # Copy the compiled module
+            nginx_modules_dir = "/etc/nginx/modules"
+            shutil.copy("objs/ngx_http_modsecurity_module.so", nginx_modules_dir)
 
-        print(f"NGINX ModSecurity Connector {version} has been downloaded and compiled successfully.")
+            print(f"NGINX ModSecurity Connector {version} has been downloaded and compiled successfully.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    
-      
+    finally:
+        # Clean up: Remove the downloaded directory
+        shutil.rmtree(download_dir, ignore_errors=True)
 
 if __name__ == "__main__":
     download_compile_nginx_modsecurity_connector()
