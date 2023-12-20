18a2b5fbbf7d9155ceb70bfca6b17d76d887657e
twink <twinkleantonysby@gmail.com>
2023-12-20 14:53:53 +0000

    nginx connector

--- Main.py
+++ Main.py
@@ -23,6 +23,39 @@ def install_modsecurity_nginx():
     except Exception as e:
         print(f"Error during ModSecurity installation: {e}")
 
+import subprocess
+
+
+def compile_modsecurity_nginx_connector():
+    try:
+        # Clone the ModSecurity-nginx repository
+        subprocess.run(['git', 'clone', '--depth', '1', 'https://github.com/SpiderLabs/ModSecurity-nginx.git'])
+
+        # Determine NGINX version
+        nginx_version_process = subprocess.run(['nginx', '-v'], capture_output=True, text=True)
+        nginx_version = nginx_version_process.stdout.split('/')[1].strip()
+
+        # Download NGINX source code
+        nginx_source_url = f'http://nginx.org/download/nginx-{nginx_version}.tar.gz'
+        subprocess.run(['wget', nginx_source_url])
+        subprocess.run(['tar', 'zxvf', f'nginx-{nginx_version}.tar.gz'])
+
+        # Compile the dynamic module
+        subprocess.run(['cd', f'nginx-{nginx_version}'])
+        subprocess.run(['./configure', '--with-compat', f'--add-dynamic-module=../ModSecurity-nginx'])
+        subprocess.run(['make', 'modules'])
+        subprocess.run(['cp', 'objs/ngx_http_modsecurity_module.so', '/etc/nginx/modules'])
+
+        print("ModSecurity connector for NGINX compiled and installed successfully.")
+    except Exception as e:
+        print(f"Error during compilation: {e}")
+    finally:
+        # Clean up: Remove the cloned repositories and downloaded files
+        subprocess.run(['rm', '-rf', 'ModSecurity-nginx', f'nginx-{nginx_version}', f'nginx-{nginx_version}.tar.gz'])
+
+
+
+
 
 def main():
     while True:
@@ -35,6 +68,9 @@ def main():
 
         if choice == '1':
             install_modsecurity_nginx()
+            compile_modsecurity_nginx_connector()
+
+
         elif choice == '2':
             print('hi')
 
