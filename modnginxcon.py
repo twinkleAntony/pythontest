af2bf3ce2a934c97fc9f499047e7c25ae8ee9578
twink <twinkleantonysby@gmail.com>
2023-12-21 12:24:35 +0000

    nginx connector

--- modnginxcon.py
+++ modnginxcon.py
@@ -1,8 +1,6 @@
 import os
-import shutil
 import subprocess
-import tarfile
-import urllib.request
+
 
 def compile_modsecurity_nginx_connector():
 
@@ -14,11 +12,26 @@ def compile_modsecurity_nginx_connector():
         # Verify that the program's working directory indeed changed
         print(f"The working directory is now '{os.getcwd()}'.")
         # Clone ModSecurity module
-        subprocess.run([ "sudo","git", "clone", "--depth", "1", "https://github.com/SpiderLabs/ModSecurity-nginx.git"])
-        # Determine NGINX version
+        clone_directory = "/usr/local/src//ModSecurity-nginx"
 
-        NGINX_VERSION =subprocess.check_output([ '`nginx -v 2>&1', '|',' awk {print $3}', '|', 'cut -d"/"', '-f', '2'])
+        # Check if the directory already exists
+        if os.path.exists(clone_directory):
+            print("ModSecurity-nginx repository is already cloned.")
+        else:
+            # Clone ModSecurity module
+            subprocess.run(
+                ["sudo", "git", "clone", "--depth", "1", "https://github.com/SpiderLabs/ModSecurity-nginx.git",
+                 clone_directory])
+            print("ModSecurity-nginx repository cloned successfully.")
+    except Exception as e:
+     print("Error during compilation: {e}")
+
+    try:
+        # Determine NGINX version
+        NGINX_VERSION = subprocess.check_output(['`nginx -v 2>&1', '|', ' awk {print $3}', '|', 'cut -d"/"', '-f', '2'])
         print("NGINX Version:", NGINX_VERSION)
+
+
         # Download NGINX source code
         nginx_url = "http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz"
         subprocess.run(['wget', nginx_url])
@@ -26,7 +39,7 @@ def compile_modsecurity_nginx_connector():
         subprocess.run(['tar', 'zxvf', 'nginx-$NGINX_VERSION.tar.gz'])
         os.chdir('nginx-$NGINX_VERSION;')
 
-        # Compile the dynamic module
+        #Compile the dynamic module
 
         subprocess.run(['./configure', '--with-compat', '--add-dynamic-module=../ModSecurity-nginx'])
         subprocess.run(['make', 'modules'])
@@ -36,7 +49,7 @@ def compile_modsecurity_nginx_connector():
 
         print("ModSecurity connector for NGINX compiled and installed successfully.")
     except Exception as e:
-        print("Error during compilation: {e}")
+     print("Error during compilation: {e}")
 
 if __name__ == "__main__":
     compile_modsecurity_nginx_connector()
